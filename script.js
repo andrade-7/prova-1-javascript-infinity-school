6bca302da142cef8eabe533ff9735d13804baa3b
Andrade <152334862+andrade-7@users.noreply.github.com>
2024-04-15 15:22:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// DECLARANDO VARIÁVEIS E ATRIBUINDO VALORES DAS NOTAS
+const n1 = Number(10)
+const n2 = Number(8)
+const n3 = Number(6)
+
+// DECLARANDO VARIÁVEIS E ATRIBUINDO VALORES AOS PESOS DAS NOTAS
+const p1 = Number(1)
+const p2 = Number(2)
+const p3 = Number(3)
+
+// ATRELANDO NOTAS AOS RESCTIVOS PESOS
+const nota1 = n1*p1
+const nota2 = n2*p2
+const nota3 = n3*p3
+
+// SOMANDO PESOS DAS NOTAS
+const peso = p1 + p2 + p3
+
+// DECLARANDO VARIÁVEL: MÉDIA 
+const media = (nota1 + nota2 + nota3) / peso
+
+// CONSOLE
+console.log(media)
